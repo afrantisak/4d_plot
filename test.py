52adaed88ff9ba375a11840de2292d9500ebe9f8
Aaron Frantisak <afrantisak@gmail.com>
2015-12-15 23:07:01 -0600

    flatten z

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ def d2((x, y), universe):
 
 def d3_to_d2((x, y, z), universe):
     xz = -z * math.sin(universe['z_angle'] * math.pi / 180)
-    yz = -z * math.cos(universe['z_angle'] * math.pi / 180)
+    yz = -z * universe['z_flat'] * math.cos(universe['z_angle'] * math.pi / 180)
     return x + xz, y + yz
 
 def d3((x, y, z)):
@@ -27,24 +27,27 @@ def draw_axes(universe):
     line_d3((0, -universe['max_axis'], 0), (0, universe['max_axis'], 0), axis_color, universe)
     line_d3((0, 0, -universe['max_axis']), (0, 0, universe['max_axis']), axis_color, universe)
 
-def draw_3d_cube(universe, size):
-    color = (255, 255, 255)
-    vertices = [(0, 0, 0), (0, 1, 0), (1, 1, 0), (1, 0, 0), (1, 0, 1), (1, 1, 1), (0, 1, 1), (0, 0, 1)]
-    edges = [(0, 1), (1, 2), (2, 3), (3, 0), (3, 4), (4, 5), (5, 2), (4, 7), (7, 6), (6, 5), (6, 1), (7, 0)]
+def draw_3d_shape(universe, vertices, edges, color):
     for edge in edges:
-        real_vert0 = [size * coord for coord in vertices[edge[0]]]
-        real_vert1 = [size * coord for coord in vertices[edge[1]]]
-        line_d3(real_vert0, real_vert1, color, universe)
+        real_vert0 = vertices[edge[0]]
+        real_vert1 = vertices[edge[1]]
+        line_d3(vertices[edge[0]], vertices[edge[1]], color, universe)
+
+def draw_3d_cube(universe, size, color):
+    vertices = [(0, 0, 0), (0, 1, 0), (1, 1, 0), (1, 0, 0), (1, 0, 1), (1, 1, 1), (0, 1, 1), (0, 0, 1)]
+    vertices = [[size * coord for coord in vertex] for vertex in vertices]
+    edges = [(0, 1), (1, 2), (2, 3), (3, 0), (3, 4), (4, 5), (5, 2), (4, 7), (7, 6), (6, 5), (6, 1), (7, 0)]
+    draw_3d_shape(universe, vertices, edges, color)
 
 def main():
     pygame.init()
-    universe = {'max_dim': 225, 'max_axis': 200, 'z_angle': -70}
+    universe = {'max_dim': 225, 'max_axis': 200, 'z_angle': -30, 'z_flat': 0.3}
     universe['screen'] = pygame.display.set_mode((2 * universe['max_dim'], 2 * universe['max_dim']), pygame.DOUBLEBUF)
     
     done = False
     while not done:
         draw_axes(universe)
-        draw_3d_cube(universe, 50)
+        draw_3d_cube(universe, 75, (255, 255, 255))
 
         universe['z_angle'] += 0
         time.sleep(0.1)
@@ -57,4 +60,3 @@ def main():
 
 main()
 
-         
